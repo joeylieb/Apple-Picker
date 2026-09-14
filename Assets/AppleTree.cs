e1bdfdbb610ea8c0731a90f720801d6c29aae7a6
Joey585 <email@joey.now>
2026-09-13 23:14:07 -0400

    Code Tree
    Create script and make tree move position depending on speed

---
+++ Assets/AppleTree.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject applePrefab;
+    public float speed = 1f;
+    public float leftAndRightEdge = 10f;
+    public float changeDirChance = 0.1f;
+    public float appleDropDelay = 1f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+        
+    }
+}
