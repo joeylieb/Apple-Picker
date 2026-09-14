a7bd44f3886234c6afb83a7087ef3b48004e4e30
Joey585 <email@joey.now>
2026-09-14 00:33:51 -0400

    High score working

--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -30,6 +30,7 @@ public class Basket : MonoBehaviour
         {
             Destroy(collidedWith);
             scoreCounter.score += 100;
+            HighScore.TRY_SET_HIGH_SCORE(scoreCounter.score);
         }
     }
 }
--- Assets/Scenes/ApplePicker.cs
+++ Assets/Scenes/ApplePicker.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour
 {
@@ -8,16 +10,19 @@ public class ApplePicker : MonoBehaviour
     public float basketBottomY = -14f;
 
     public float basketSpacingY = 2f;
+    public List<GameObject> basketList; 
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        basketList = new List<GameObject>();
         for (int i = 0; i < numBaskets; i++)
         {
             GameObject tBasketGO = Instantiate(basketPrefab);
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (basketSpacingY * i);
             tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO);
         }
     }
 
@@ -27,6 +32,16 @@ public class ApplePicker : MonoBehaviour
         foreach (GameObject apple in appleArray)
         {
             Destroy(apple);
+        }
+
+        int basketIndex = basketList.Count - 1;
+        GameObject basketGO = basketList[basketIndex];
+        basketList.RemoveAt(basketIndex);
+        Destroy(basketGO);
+
+        if (basketList.Count == 0)
+        {
+            SceneManager.LoadScene("_Scene_0");
         }
     }
 
