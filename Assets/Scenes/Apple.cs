0c440580e613badea1349666e5d0d86c2b50be31
Joey585 <email@joey.now>
2026-09-13 23:42:29 -0400

    Destroy apples
    Apples get destroyed when they fall too far

---
+++ Assets/Scenes/Apple.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+public class Apple : MonoBehaviour
+{
+    public static float bottomY = -20f;
+
+    private void Update()
+    {
+        if (transform.position.y < bottomY)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
